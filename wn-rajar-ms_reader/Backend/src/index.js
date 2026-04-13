7855889911c54e39904754eec06f926f763c49a5
Salvak <jules.clauwaert@yahoo.com>
2026-04-13 16:06:44 +0200

    open CORS temporarily to debug

--- wn-rajar-ms_reader/Backend/src/index.js
+++ wn-rajar-ms_reader/Backend/src/index.js
@@ -6,12 +6,7 @@ const articlesRoutes = require("./routes/articles");
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      const allowed = ["http://localhost:5173", process.env.FRONTEND_URL].filter(Boolean);
-      if (allowed.includes(origin)) return callback(null, true);
-      return callback(null, false);
-    },
+    origin: true,
     methods: ["GET", "POST", "DELETE"],
     credentials: true,
   }),
